69257956bd63a22efe38546ff54a99899ea5f8b4
Isabella Szemeti <issythegirl@gmail.com>
2020-11-30 04:50:13 +0000

    chore(intelark): Move to new menu system

--- intelark/intelark.py
+++ intelark/intelark.py
@@ -1,252 +1,145 @@
 import discord
-from redbot.core import commands, checks
-from redbot.core.data_manager import cog_data_path
+from redbot.core import commands
+from redbot.core.utils.menus import menu, prev_page, close_menu, next_page
 import aiohttp
 import asyncio
-import bs4
 from bs4 import BeautifulSoup as soup
 import urllib.parse
 import re
 
+CUSTOM_CONTROLS = {"⬅️": prev_page, "⏹️": close_menu, "➡️": next_page}
+
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'}
+
 __author__ = "Issy"
+
+desiredSpecs = ('ProcessorNumber','CoreCount','ThreadCount','HyperThreading','ClockSpeed','SocketsSupported','MaxTDP','AESTech','MaxMem')
+returnedSpecs = ('Url', 'ProcessorNumber', 'CoreCount', 'ThreadCount', 'HyperThreading', 'ClockSpeed', 'SocketsSupported', 'MaxTDP', 'AESTech', 'MaxMem', 'VTD', 'ClockSpeedMax')
+
+async def make_soup(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers=headers, allow_redirects=True) as data:
+            data_text = await data.text()
+            page_soup = soup(data_text, "html.parser")
+            await session.close()
+    return page_soup
 
 class IntelArk(commands.Cog):
     """Search for Intel CPUs"""
 
     def __init__(self, client):
         self.client = client
-        self.intelBlue = 0x0071C5
-        self.specialQueries = {
-        "@everyone": "Hah. Nice try. Being very funny. Cheeky cunt.",
-        "@here": "Hilarious, I'm reporting you to the mods.",
-        ":(){ :|: & };: -": "This is a python bot, not a bash bot you nimwit."
-        }
-        self.headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'}
+        self.intel_blue = 0x0071C5
 
-    # Events
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print('Intel Ark cog online')
+# Commands
 
-    @commands.command()
-    async def test(self, ctx, *args):
-        await ctx.send(args)
-        print(args)
-
-    @commands.command()
-    async def ark(self, ctx, *searchTerm):
+    @commands.command(name='ark')
+    async def _ark(self, ctx: discord.Context, *, search_term: str):
         """Search for Intel CPUs"""
-        for word in searchTerm:
-            if word in self.specialQueries:
-                await ctx.send(embed=discord.Embed(colour=self.intelBlue,description=self.specialQueries[word]))
-                return
+        # Check for special queries
+        response = self.get_response(ctx, search_term)
+        if response:
+            await ctx.send(embed=response)
+            return
+
+        results: tuple[discord.Embed] = await self.get_search_results(search_term)
+        if not results:
+            embed = discord.Embed(description=f"No results found for `{search_term.replace('`','``')}`", colour=self.intel_blue)
+            await ctx.send(embed=embed)
+            return
+        if len(results) == 1:
+            await ctx.send(embed=results[0])
+            return
+        await menu(ctx, pages=results, controls=CUSTOM_CONTROLS, message=None, page=0, timeout=120)
+
+# Helper functions
+
+    def get_response(self, ctx: discord.Context, search_term: str) -> discord.Embed:
+        """Get quirky responses for special triggers"""
+        special_queries = {
+            "@everyone": "Hah. Nice try. Being very funny. Cheeky cunt.",
+            "@here": "Hilarious, I'm reporting you to the mods.",
+            ":(){ :|: & };: -": "This is a python bot, not a bash bot you nimwit."
+        }
+        for word in search_term.split(' '):
+            if word in special_queries:
+                return discord.Embed(colour = self.intel_blue, description = special_queries[word])
             if re.compile('<@![0-9]{18}>').match(word):
-                await ctx.send(embed=discord.Embed(colour=self.intelBlue,description=f"<@{ctx.author.id}> pong!"))
-                return
+                return discord.Embed(colour = self.intel_blue, description = f"<@{ctx.author.id}> pong!")
+        return False
 
-        indexModifier = re.compile('r=[0-9]')
-        if indexModifier.match(searchTerm[-1]):
-            regexMatched = True
-            matchedString = searchTerm[-1]
-            searchTerm = " ".join(searchTerm[:-1])
-        else:
-            regexMatched = False
-            searchTerm = " ".join(searchTerm)
-        cleanSearchTerm = urllib.parse.quote(searchTerm) # clean up the search term for the url
-
-        url = f"https://ark.intel.com/content/www/us/en/ark/search.html?_charset_=UTF-8&q={cleanSearchTerm}"
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url, headers=self.headers, allow_redirects=True) as data:
-                dataText = await data.text()
-                page_soup = soup(dataText, "html.parser")
-                await session.close()
-
+    async def get_urls(self, page_soup: soup) -> tuple:
         if (page_soup.find("input",{"id":"FormRedirectUrl"})): # if only one result
             url = page_soup.find("input",{"id":"FormRedirectUrl"}).get('value')
-            url = f"https://ark.intel.com{url}"
-            index = {}
-            index['min'] = 1
-            index['current'] = 1
-            index['max'] = 1
-            data = await self.get_cpu_data(url)
-            embed = await self.make_ark_embed(data,index)
-            await ctx.send(embed=embed)
-            return
-
+            return [f"https://ark.intel.com{url}"]
         if (page_soup.find("h2",text="No products matching your request were found.")): # if no products found
-            embed = discord.Embed(colour=self.intelBlue,description=f"No results found for `{searchTerm.replace('`','``')}`")
-            await ctx.send(embed=embed)
-            return
-
+            return []
         # build list of URLs
         results = page_soup.findAll("div",{"class":"search-result"})
         urls = []
         ignore = ['generation','ethernet','wireless','products formerly','heat sink']
         for item in results:
-            trigger = 0
+            trigger = False
             itemTitle = item.find("h4",{"class":"result-title"}).find("a").contents[0].strip().lower()
+            # If certain keywords are in the product body, skip this product
             for word in ignore:
                 if word in itemTitle:
-                    trigger = 1
-                    break
-            if trigger == 1:
-                continue
-            else:
-                url = item.find("h4",{"class":"result-title"}).find("a").get('href')
-                url = f"https://ark.intel.com{url}"
+                    trigger = True
+            if not trigger:
+                url_suffix = item.find("h4",{"class":"result-title"}).find("a").get('href')
+                url = f"https://ark.intel.com{url_suffix}"
                 urls.append(url)
+        return tuple(urls)
 
-        index = {}
-        index['min'] = 1
-        if regexMatched == True:
-            if int(matchedString.split('r=')[1]) <= 0:
-                index['current'] = int(matchedString.split('r=')[1]) * -1
-            else:
-                index['current'] = int(matchedString.split('r=')[1])
-        else:
-            index['current'] = 1
-        index['max'] = len(results)
-        if index['current'] > index['max']:
-            index['current'] = index['max']
-
-        # make embed
-        data = await self.get_cpu_data(urls[index['current']-1])
-        embed = await self.make_ark_embed(data,index)
-
-        if index['min'] == index['max']: # if there is only one result
-            await ctx.send(embed=embed)
-            return
-
-        messageObject = await ctx.send(embed=embed)
-        if (index['current'] == index['min']) and (index['current'] != index['max']): # if this is the first result, and there are multiple
-            allowedEmojis = ['▶']
-            for emoji in allowedEmojis:
-                await messageObject.add_reaction(emoji)
-        if (index['current'] != index['min']) and (index['current'] != index['max']): # if this is a middle result
-            allowedEmojis = ['◀','▶']
-            for emoji in allowedEmojis:
-                await messageObject.add_reaction(emoji)
-        if (index['current'] == index['max']) and (index['current'] != index['min']): # if this is the last result
-            allowedEmojis = ['◀']
-            for emoji in allowedEmojis:
-                await messageObject.add_reaction(emoji)
-
-        async def editResult(urls, index, messageObject):
-            data = await self.get_cpu_data(urls[index['current']-1])
-            embed = await self.make_ark_embed(data,index)
-            await messageObject.edit(embed=embed)
-            if (index['current'] == index['min']) and (index['current'] != index['max']): # if this is the first result, and there are multiple
-                allowedEmojis = ['▶']
-                for emoji in allowedEmojis:
-                    await messageObject.add_reaction(emoji)
-            if (index['current'] != index['min']) and (index['current'] != index['max']): # if this is a middle result
-                allowedEmojis = ['◀','▶']
-                for emoji in allowedEmojis:
-                    await messageObject.add_reaction(emoji)
-            if (index['current'] == index['max']) and (index['current'] != index['min']): # if this is the last result
-                allowedEmojis = ['◀']
-                for emoji in allowedEmojis:
-                    await messageObject.add_reaction(emoji)
-
-            def reaction_info_check(reaction,user):
-                return user == ctx.author and reaction.message.id == messageObject.id
-
-            try:
-                reaction, user = await self.client.wait_for('reaction_add', timeout=120.0, check=reaction_info_check)
-            except asyncio.TimeoutError:
-                await messageObject.clear_reactions()
-                return
-            else:
-                # Okay, the user has reacted with an emoji, let's find out which one!
-                if reaction.emoji in allowedEmojis:
-                    if reaction.emoji == '▶':
-                        index['current'] = index['current'] + 1
-                        await messageObject.clear_reactions()
-                        await editResult(urls,index,messageObject)
-                    if reaction.emoji == '◀':
-                        index['current'] = index['current'] - 1
-                        await messageObject.clear_reactions()
-                        await editResult(urls,index,messageObject)
-
-        def reaction_info_check(reaction,user):
-            return user == ctx.author and reaction.message.id == messageObject.id
-
-        try:
-            reaction, user = await self.client.wait_for('reaction_add', timeout=120.0, check=reaction_info_check)
-        except asyncio.TimeoutError:
-            await messageObject.clear_reactions()
-            return
-        else:
-            # Okay, the user has reacted with an emoji, let's find out which one!
-            if reaction.emoji in allowedEmojis:
-                if reaction.emoji == '▶':
-                    index['current'] = index['current'] + 1
-                    await messageObject.clear_reactions()
-                    await editResult(urls,index,messageObject)
-                if reaction.emoji == '◀':
-                    index['current'] = index['current'] - 1
-                    await messageObject.clear_reactions()
-                    await editResult(urls,index,messageObject)
-
-    async def get_cpu_data(self, url):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url, headers=self.headers) as data:
-                dataText = await data.text()
-                page_soup = soup(dataText, "html.parser")
-                await session.close()
+    def get_cpu_data(self, page: soup) -> dict:
         specs = {}
-        desiredSpecs = ['ProcessorNumber','CoreCount','ThreadCount','HyperThreading','ClockSpeed','SocketsSupported','MaxTDP','AESTech']
-        specs['Url'] = url
-        for specItem in desiredSpecs:
+        specs['Url'] = page.find('meta', {'property': 'og:url'})['content']
+        for spec_item in desiredSpecs:
             try:
-                specs[specItem] = page_soup.find("span",{"class":"value","data-key":specItem}).contents[0].strip()
+                specs[spec_item] = page.find("span",{"class":"value","data-key":spec_item}).contents[0].strip()
             except AttributeError:
-                specs[specItem] = None
-        # specs['ProcessorNumber'] = page_soup.find("span",{"class":"value","data-key":"ProcessorNumber"}).contents[0].strip()
-        # specs['CoreCount'] = page_soup.find("span",{"class":"value","data-key":"CoreCount"}).contents[0].strip()
-        # specs['ThreadCount'] = page_soup.find("span",{"class":"value","data-key":"ThreadCount"}).contents[0].strip()
-        # specs['HyperThreading'] = page_soup.find("span",{"class":"value","data-key":"HyperThreading"}).contents[0].strip()
-        # specs['ClockSpeed'] = page_soup.find("span",{"class":"value","data-key":"ClockSpeed"}).contents[0].strip()
-        # specs['SocketsSupported'] = page_soup.find("span",{"class":"value","data-key":"SocketsSupported"}).contents[0].strip()
-        # specs['MaxTDP'] = page_soup.find("span",{"class":"value","data-key":"MaxTDP"}).contents[0].strip()
-        # specs['AESTech'] = page_soup.find("span",{"class":"value","data-key":"AESTech"}).contents[0].strip()
-        try:
-            specs['VTD'] = page_soup.find("span",{"class":"value","data-key":"VTD"}).contents[0].strip()
-        except AttributeError:
-            specs['VTD'] = None
-        try:
-            specs['ClockSpeedMax'] = page_soup.find("span",{"class":"value","data-key":"ClockSpeedMax"}).contents[0].strip()
-        except AttributeError: # If the CPU doesn't have a boost frequency
-            specs['ClockSpeedMax'] = None
+                specs[spec_item] = None
+        for spec_item in ['VTD','ClockSpeedMax']:
+            try:
+                specs[spec_item] = page.find("span", {"class": "value", "data-key": spec_item}).contents[0].strip()
+            except AttributeError:
+                specs[spec_item] = None
         return specs
 
-    async def make_ark_embed(self, data, index):
-        embed = discord.Embed(colour=self.intelBlue)
-        embed.set_author(name='Ark Search Result',url=data['Url'])
+    def make_ark_embed(self, data: dict, index: dict = {}) -> discord.Embed:
+        embed = discord.Embed(title='Ark Search Result', url=data['Url'], colour=self.intel_blue)
         embed.add_field(name='Product Name',value=f"[{data['ProcessorNumber']}]({data['Url']})",inline=True)
         if data['ClockSpeed'] != None:
             if data['ClockSpeedMax'] != None:
                 embed.add_field(name='Clock Speed',value=f"{data['ClockSpeed']} / {data['ClockSpeedMax']}",inline=True)
             else:
                 embed.add_field(name='Clock Speed',value=data['ClockSpeed'],inline=True)
-        if data['HyperThreading'] != None:
-            if data['HyperThreading'] == 'No':
-                embed.add_field(name='Cores',value=data['CoreCount'],inline=True)
-            elif data['HyperThreading'] == 'Yes':
-                embed.add_field(name='Cores/Threads',value=f"{data['CoreCount']} / {data['ThreadCount']}",inline=True)
-        if (data['HyperThreading'] == None) and (data['CoreCount'] != None):
-            embed.add_field(name='Cores',value=data['CoreCount'],inline=True)
+        core_thread_value = f"{data['CoreCount']} / {data['CoreCount']}" if data['HyperThreading'] == 'No' or data['HyperThreading'] == None else f"{data['CoreCount']} / {data['ThreadCount']}"
+        embed.add_field(name='Cores/Threads', value=core_thread_value, inline=True)
         if data['MaxTDP'] != None:
-            embed.add_field(name='TDP',value=data['MaxTDP'],inline=True)
+            embed.add_field(name='TDP', value=data['MaxTDP'], inline=True)
         if data['VTD'] != None:
-            embed.add_field(name='VTD',value=data['VTD'],inline=True)
+            embed.add_field(name='VTD', value=data['VTD'], inline=True)
         if data['AESTech'] != None:
-            embed.add_field(name='AES Tech',value=data['AESTech'],inline=True)
+            embed.add_field(name='AES Tech', value=data['AESTech'], inline=True)
         if data['SocketsSupported'] != None:
-            embed.add_field(name='Sockets',value=data['SocketsSupported'],inline=True)
-        if (index['max'] != 1):
-            embed.set_footer(text=f"{index['current']} of {index['max']}")
+            embed.add_field(name='Sockets', value=data['SocketsSupported'], inline=True)
+        if index:
+            embed.set_footer(text=f"{index['current']+1} of {index['max']}")
         return embed
 
-def setup(client):
-    client.add_cog(IntelArk(client))
+    async def get_search_results(self, search_term: str) -> tuple:
+        url: str = f"https://ark.intel.com/content/www/us/en/ark/search.html?_charset_=UTF-8&q={search_term}"
+        search_page: soup = await make_soup(url)
+        urls: tuple[str] = await self.get_urls(search_page)
+        if not urls:
+            return ()
+        pages: list[soup] = []
+
+        for url in urls:
+            page: soup = await make_soup(url)
+            pages.append(page)
+
+        all_cpu_data: list[dict] = [self.get_cpu_data(i) for i in pages]
+        embeds = [self.make_ark_embed(all_cpu_data[i], {'min': '0', 'current': i, 'max': len(all_cpu_data)}) for i in range(len(all_cpu_data))]
+        return tuple(embeds)
